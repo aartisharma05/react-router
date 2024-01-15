a58e49d4bcb52731c0c137e37d6741fd29cfb91b
aartisharma05 <aartisharma.official1@gmail.com>
2024-01-15 16:45:02 +0530

    contact route

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
     element:  <Root />,
     errorElement: <ErrorPage />,
   },
+    {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
