5727f3ccb4ba2ceb8d0f587e2217559c776f659c
aartisharma05 <aartisharma.official1@gmail.com>
2024-01-15 17:01:29 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from './routes/contact'
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -12,11 +14,14 @@ const router = createBrowserRouter([
     path: "/",
     element:  <Root />,
     errorElement: <ErrorPage />,
-  },
-    {
+    children: [
+        {
     path: "contacts/:contactId",
     element: <Contact />,
   },
+    ]
+  },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
